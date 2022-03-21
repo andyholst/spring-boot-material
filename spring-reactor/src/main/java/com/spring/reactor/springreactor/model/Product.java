6d72a3e165ef52103aa2d5a3ee48f4fe3380eb3e
Andy Holst <andy.holst85@gmail.com>
2022-03-22 00:02:31 +0100

    Added editorconfig and Lombok dependency.

--- spring-reactor/src/main/java/com/spring/reactor/springreactor/model/Product.java
+++ spring-reactor/src/main/java/com/spring/reactor/springreactor/model/Product.java
@@ -1,7 +1,18 @@
 package com.spring.reactor.springreactor.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Getter
+@Setter
+@NoArgsConstructor
 public class Product {
+	@Id
+	private String id;
+	private String name;
+	private Double price;
 }
